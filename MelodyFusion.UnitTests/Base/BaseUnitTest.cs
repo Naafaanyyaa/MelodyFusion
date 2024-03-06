db74684b014783dcf8894cf10be196c19e4a35e6
Artem Pushkar <skrillex55214@gmail.com>
2024-03-06 14:29:28 +0200

    Added base unit tests setup

---
+++ MelodyFusion.UnitTests/Base/BaseUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace MelodyFusion.UnitTests.Base
+{
+    public class BaseUnitTest
+    {
+        public static IMapper IMapper { get; private set; }
+
+        static BaseUnitTest()
+        {
+            MapperConfiguration config = new(cfg => cfg.AddProfile<BLL.Mappings.AutoMapper>());
+            IMapper = config.CreateMapper();
+        }
+
+        public ILoggerFactory GetMockedLogger(ILogger logger = null)
+        {
+            Mock<ILoggerFactory> loggerFactoryMock = new();
+            loggerFactoryMock.Setup(f => f.CreateLogger(It.IsAny<string>())).Returns(logger ?? new Mock<ILogger>().Object);
+
+            return loggerFactoryMock.Object;
+        }
+
+        public void AssertEqualCollections<T>(List<T> expectedCollection, List<T> actualCollection)
+        {
+            Assert.Equal(expectedCollection.Count, actualCollection.Count);
+
+            int i = 0;
+            Assert.Collection(actualCollection,
+                actualCollection.Select(actualItem => (Action<T>)(item => Assert.Equal(expectedCollection[i++], item))).ToArray()
+            );
+        }
+    }
+}
---
+++ MelodyFusion.UnitTests/Services/RegistrationServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using MelodyFusion.BLL.Exceptions;
+using MelodyFusion.BLL.Interfaces;
+using MelodyFusion.BLL.Models.Request;
+using MelodyFusion.DLL.Entities.Identity;
+using MelodyFusion.DLL.Enums;
+using MelodyFusion.DLL.Interfaces;
+using MelodyFusion.UnitTests.Base;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace MelodyFusion.UnitTests.Services
+{
+    public class RegistrationServiceTests : BaseUnitTest
+    {
+        private readonly Mock<UserManager<UserDto>> _userManagerMock = new();
+        private readonly Mock<IAuthenticationStatisticRepository> _authenticationStatisticRepositoryMock = new();
+        private readonly Mock<IRegistrationService> _registrationService = new();
+        private readonly Mock<IRegistrationService> _registrationServiceMock = new Mock<IRegistrationService>();
+
+        public RegistrationServiceTests()
+        {
+        
+        }
+        [Fact]
+        public async Task Should_Throw_ValidationException_When_Register()
+        {
+            var request = new RegistrationRequest
+            {
+                UserName = "existingUser",
+                Email = "existing@example.com",
+                Password = "password",
+                Role = RoleEnum.User
+            };
+
+            _registrationServiceMock.Setup(x => x.Registration(request))
+                .ThrowsAsync(new ValidationException("User with such username or email already exists"));
+            ;
+
+        }
+    }
+}
